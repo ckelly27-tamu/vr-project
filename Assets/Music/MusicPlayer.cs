3215d14fe9fc281a3912a9a13a6e293ddb14a5c0
Infilit <connor.kelly@tamu.edu>
2026-03-31 13:00:48 -0500

    Add music

--- Assets/Music/MusicPlayer.cs
+++ Assets/Music/MusicPlayer.cs
@@ -6,21 +6,39 @@ public class MusicPlayer : MonoBehaviour
 {
     private bool isPlaying = false;
     private Animator animator;
-    public GameObject vinyl;
-    public TextMeshProUGUI statusText;
-    public TextMeshProUGUI buttonText;
-    public Button PlayPauseButton;
-    public GameObject musicObject;
+    public GameObject animatedVinyl;
+    public GameObject playingVinyl;
+    private Vinyl vinylComponent;
     private AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        audioSource = musicObject.GetComponent<AudioSource>();
-        animator = vinyl.GetComponent<Animator>();
-        PlayPauseButton.onClick.AddListener(PlayPause);
-        isPlaying = true;
-        VPlay();
+        isPlaying = false;
+    }
+
+    void Awake()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        playingVinyl = other.gameObject;
+        audioSource = playingVinyl.GetComponent<AudioSource>();
+
+        if (audioSource != null)
+        {
+            VPlay();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (playingVinyl == other.gameObject)
+        {
+            VPause();
+        }
     }
 
     // Update is called once per frame
@@ -29,31 +47,16 @@ public class MusicPlayer : MonoBehaviour
         
     }
 
-    void PlayPause()
-    {
-        if (isPlaying)
-        {
-            isPlaying = false;
-            VPause();
-        } else
-        {
-            isPlaying = true;
-            VPlay();
-        }
-    }
-
     void VPlay()
     {
+        isPlaying = true;
         animator.SetBool("IsPlaying", true);
-        statusText.text = "Playing";
-        buttonText.text = "Pause";
         audioSource.Play();
     }
     void VPause()
     {
+        isPlaying = false;
         animator.SetBool("IsPlaying", false);
-        statusText.text = "Paused";
-        buttonText.text = "Play";
-        audioSource.Pause();
+        audioSource.Stop();
     }
 }
