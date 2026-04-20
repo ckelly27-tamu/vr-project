071f5a5e9b92814f3c34091853ec4fe9ed539e9a
Infilit <connor.kelly@tamu.edu>
2026-04-19 19:32:54 -0500

    a3

--- Assets/Challenges/JinglePlay.cs
+++ Assets/Challenges/JinglePlay.cs
@@ -2,27 +2,31 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class PaintingButton : MonoBehaviour
+public class JinglePlay : MonoBehaviour
 {
+    public AudioSource audioSource;
+    
     private XRSimpleInteractable interactable;
-
-    public PaintingChallenge paintingChallenge;
-    public Painting representedPainting;
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         interactable = GetComponent<XRSimpleInteractable>();
         interactable.selectEntered.AddListener(OnButtonPressed);
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     void OnButtonPressed(SelectEnterEventArgs args)
     {
-        paintingChallenge.TestPainting(representedPainting);
+        audioSource.Play();
     }
 
     void OnDestroy()
     {
         interactable.selectEntered.RemoveListener(OnButtonPressed);
     }
-
 }
--- Assets/Music/Vinyl.cs
+++ Assets/Music/Vinyl.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Vinyl : MonoBehaviour
 {
+    public int id = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
